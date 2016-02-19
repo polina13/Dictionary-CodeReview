383907c4e4922c6f45ed122b3d534f28d242401f
Polina Nenchev <polina.mihova@yahoo.com>
2016-02-19 08:12:18 -0800

    Create and pass test for returning all instances of a word and once done revome word from the list

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
 
 public class Word {
-  // private static ArrayList<Definition> instances = new ArrayList<Definition>();
+  private static ArrayList<Word> instances = new ArrayList<Word>();
 
   private String mWord;
+  private int mId;
 
   public Word (String word) {
     mWord = word;
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getWord() {
     return mWord;
+  }
+
+  public Integer getId() {
+    return mId;
+  }
+
+  public static ArrayList<Word> all() {
+    return instances;
+  }
+  public static void clear() {
+    instances.clear();
   }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -10,4 +10,25 @@ public class WordTest {
     Word testWord = new Word("home");
     assertEquals("home", testWord.getWord());
   }
+
+  @Test
+  public void getId_returnsWordId() {
+    Word testWord = new Word("home");
+    assertTrue(Word.all().size() == testWord.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfWord_true() {
+    Word firstWord = new Word("home");
+    Word secondWord = new Word("home");
+    assertTrue(Word.all().contains(firstWord));
+    assertTrue(Word.all().contains(secondWord));
+  }
+
+  @Test
+  public void clear_removesAllWordsFromArrayList() {
+    Word testWord = new Word("home");
+    Word.clear();
+    assertEquals(Word.all().size(), 0);
+  }
 }
